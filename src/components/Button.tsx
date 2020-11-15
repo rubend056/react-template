4175a455be8a735d728de4e3217f999e3138410a
RubenD <rubendariopm14@gmail.com>
2020-11-15 15:08:07 -0500

    using rebass

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,77 +1,23 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { css, useTheme, Theme } from '@emotion/react'
-import StyledComponent from '../styles/StyledComponent';
+import styled from '@emotion/styled'
+import {Button as RButton} from 'rebass'
+import {Switch as RSwtich} from '@rebass/forms'
 import { palette, useS } from '../themes'
 
-export type ButtonProps =
-	StyledComponent & {
-		width?: number
-	}
-
-
-// ? ############## BUTTON #################
-class Button extends React.Component<ButtonProps>{
-	render() {
-		return (
-			<button
-				css={(t: Theme) => useS(t, this, (p) => ({
-					background: p.cd,
-					color: p.cd_t,
-					transition: `all ${t.animations_length}s`,
-					borderRadius: t.space[1],
-					padding: t.space[2],
-					'&:hover': {
-						background: p.cl,
-						color: p.cl_t,
-					}
-				}))}
-				{...this.props}
-			>
-				{this.props.children}
-			</button>
-		)
-	}
-};
-
-// ? ############## TOGGLE BUTTON #################
-
-class ButtonToggle extends React.Component<ButtonProps>{
-	render() {
-		return (
-			<div
-				className="switch"
-				css={(t: Theme) => useS(t, this, (p) => ({
-					// background: p.cd,
-					// color: p.cd_t,
-					
-					height: 30,
-					width: 60,
-					
-					'&:hover': {
-						// background: p.cl,
-						// color: p.cl_t,
-					},
-					'&.slider:before':{
-						transition: `${t.animations_length}s`,
-					},
-					'&input:checked + .slider)':{
-						backgroundColor: p.c,
-					},
-					'&input:focus + .slider': {
-						boxShadow: `0 0 1px ${p.c}`,
-					},
-				}))}
-			>
-				<input type="checkbox" />
-				<span className="slider"></span>
-				{this.props.children}
-			</div>
-		)
-	}
+export interface ButtonProps{
+	
 }
-// const StyledButtonToggle = styled(ButtonToggle)`
-// 	${buttonStyle};
-// `
 
-export { Button, ButtonToggle }
+export const Button:FunctionComponent<ButtonProps> = (props) => {
+	return <RButton p={2} m={1} {...props}>
+		{props.children}
+	</RButton>
+}
+
+export const ButtonToggle:FunctionComponent<ButtonProps> = (props) => {
+	return <RSwtich p={2} m={1} {...props}>
+		{props.children}
+	</RSwtich>
+}
--- src/components/ComponentsTest.tsx
+++ src/components/ComponentsTest.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonToggle } from './Button'
 
 const ComponentsTest = (props) => (
 	<div>
-		<Button palette="secondary">Click Me</Button>
+		<Button >Click Me</Button>
 		<ButtonToggle>Toggle Me</ButtonToggle>
 	</div>
 )
